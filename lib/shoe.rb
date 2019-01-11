fe84cc719cb1c75854a2b5916050ceb15796ccca
Diana Badas <diana.badas@gmail.com>
2019-01-11 12:41:23 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,27 +1,8 @@
 class Shoe
-
+  attr_accessor :color, :size, 
+  attr_reader :brand
   def initialize(brand)
     @brand = brand
-  end
-
-  def brand
-    @brand
-  end
-
-  def color=(color)
-    @color = color
-  end
-
-  def color
-    @color
-  end
-
-  def size=(size)
-    @size = size
-  end
-
-  def size
-    @size
   end
 
   def material=(material)
